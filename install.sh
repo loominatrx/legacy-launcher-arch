4fa23032d694a6802293332be3543a67773f57ec
Mas Gading <loominatr1x@gmail.com>
2023-12-27 09:18:51 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,37 +2,32 @@
 
 [[ "$(whoami)" = "root" ]] && echo "Script must be run as a normal user." && exit
 
+[[ "$(whereis paru)" = "paru:" ]] && echo "paru must be installed to use AUR packages." && exit
+
 # Colors
 GREEN="\033[1;32m"
 END="\033[0m"
 
-## wget is for downloading TLuncher directly from their website.
-## All the other packages are java packages recommended by TLauncher
-## (They use java 8, but instead i'm installing the most recent java version).
 # Downloading packages
 pkgs=(
-	wget
-	jdk-openjdk
-	jre-openjdk
-	jre-openjdk-headless
-	java-openjfx
+	zulu-21-bin
 )
 sudo pacman -S --noconfirm --needed "${pkgs[@]}"
 
-# Downloading TLauncher
-echo -e "\n${GREEN}Downloading TLauncher...${END}"
+# Downloading Legacy Launcher
+echo -e "\nDownloading ${GREEN}Legacy Launcher${END}..."
 
-wget https://tlauncher.org/jar -P src/
-unzip src/jar -d src/tlauncher.d
-mv src/tlauncher.d/*.jar src/tlauncher.d/tlauncher.jar
+curl https://llaun.ch/jar -o src/jar
+unzip src/jar -d src/llaunch.d
+mv src/llaunch.d/*.jar src/llaunch.d/llaunch.jar
 
 # Moving TLauncher files
-echo -e "\n${GREEN}Installing TLauncher...${END}"
-sudo mkdir /usr/share/tlauncher
-sudo mv src/tlauncher.d/* /usr/share/tlauncher
+echo -e "\nInstalling ${GREEN}Legacy Launcher${END}..."
+sudo mkdir /usr/share/llaunch
+sudo mv src/llaunch.d/* /usr/share/llaunch
 
-sudo mv src/tlauncher /usr/bin/
-sudo mv src/tlauncher.png /usr/share/icons
-sudo mv src/tlauncher.desktop /usr/share/applications
+sudo mv src/llaunch /usr/bin/
+sudo mv src/llaunch.png /usr/share/icons
+sudo mv src/llaunch.desktop /usr/share/applications
 
-echo -e "\n${GREEN}Installation Complete!${END}"
+echo -e "\n\nInstallation Complete!"
